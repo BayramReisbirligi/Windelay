081c8d38da159b6db56d965f4c7c85d85a4bde81
Bayram Reis <bayramreisbirligi@gmail.com>
2025-09-30 14:06:47 +0300

    .

---
+++ Utilities/Enums/DelayType.cs
@@ -0,0 +1,16 @@
+﻿namespace ReisProduction.Windelay.Utilities.Enums;
+public enum DelayType : ushort
+{
+    HybridDelay = 0x01,
+    HighResSpin = 0x02,
+    SpinDelay = 0x03,
+    WaitableTimer = 0x04,
+    HighResSleep = 0x05,
+    SleepDelay = 0x06,
+    TaskDelay = 0x07,
+    TaskDelayWait = 0x08,
+    EventWaitHandle = 0x09,
+    TimersTimerDelay = 0x0A,
+    TimerDelay = 0x0B,
+    FormsTimerDelay = 0x0C
+}
---
+++ Utilities/Records.cs
@@ -0,0 +1,7 @@
+﻿using ReisProduction.Windelay.Utilities.Enums;
+namespace ReisProduction.Windelay.Utilities;
+public record DelayAction(
+    int DelayMilisecond,
+    CancellationToken Token,
+    DelayType DelayType = DelayType.HybridDelay
+) : IDelayAction;
