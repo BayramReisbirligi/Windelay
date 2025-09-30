ab82910fce106d4e44f2d14c851bd5daef71e52e
Bayram Reis <bayramreisbirligi@gmail.com>
2025-09-30 14:06:34 +0300

    .

--- Utilities/Interfaces.cs
+++
@@ -1,8 +0,0 @@
-﻿using ReisProduction.Windelay.Utilities.Enums;
-namespace ReisProduction.Windelay.Utilities;
-public interface IDelayAction
-{
-    int DelayMilisecond { get; }
-    CancellationToken Token { get; }
-    DelayType DelayType { get; }
-}
--- Utilities/Records.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReisProduction.Windelay.Utilities.Enums;
-namespace ReisProduction.Windelay.Utilities;
-public record DelayAction(
-    int DelayMilisecond,
-    CancellationToken Token,
-    DelayType DelayType = DelayType.HybridDelay
-) : IDelayAction;
